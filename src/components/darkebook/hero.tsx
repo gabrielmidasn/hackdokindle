ec67226289e1bef6e997878cec1a3b0c09016c4d
Magaiver Do pixo <magaiverdopixo@gmail.com>
2025-10-30 19:09:44 +0000

    https://imgur.com/b0FqFsO

--- src/components/darkebook/hero.tsx
+++ src/components/darkebook/hero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Hero() {
   return (
     <header className="w-full flex justify-center py-8 bg-charcoal-black">
-      <Image src="https://i.imgur.com/gA3J4XJ.png" alt="Darkebook Logo" width={188} height={50} />
+      <Image src="https://i.imgur.com/b0FqFsO.png" alt="Darkebook Logo" width={200} height={50} />
     </header>
   );
 }
